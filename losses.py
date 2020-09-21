7f89a43d3e74e8b0b61ceb709cbb3049f6a75141
Carsten Drösser <carsten.droesser@gmail.com>
2020-09-21 21:26:25 +0200

    new comments

--- losses.py
+++ losses.py
@@ -1,8 +1,10 @@
+import numpy
 import tensorflow as tf
 import tensorflow.keras.backend as K
 
 
 def loss_fn(y_truth, y_predicted):
+    # gets called per depthmap and not per batch
     max_depth_value = K.max(y_truth)
 
     # pointwise depth error
@@ -13,7 +15,8 @@ def loss_fn(y_truth, y_predicted):
     dy_predicted, dx_predicted = tf.image.image_gradients(y_predicted)
     loss_gradients = K.mean(K.abs(dy_predicted - dy_truth) + K.abs(dx_predicted - dx_truth), axis=-1)
 
-    # Structural similarity (SSIM) index
+    # Structural similarity (SSIM) index lim -> 0
+    # ssim can be in range [-1, 1] so it is clipped to 1
     loss_similarity = K.clip((1 - tf.image.ssim(y_truth, y_predicted, max_depth_value)) * 0.5, 0, 1)
 
     # Weights
